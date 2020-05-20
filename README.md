d11e2a248bfb7732c20533013491eb7b130828bc
Yokiijay <yokiijay@gmail.com>
2020-05-20 11:56:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vue-raw-box
+ A vue component from Vue Raw UI library.
---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Box from 'vue-raw-box'
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Box
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <Box />
+  </div>
+</template>
